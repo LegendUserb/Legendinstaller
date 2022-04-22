dda5b4070555d9bea82e5701f3d562e28a0a0c52
Nusret <89344065+nusrte@users.noreply.github.com>
2022-04-22 20:30:15 +0400

    Update __init__.py

--- merlin_installer/__init__.py
+++ merlin_installer/__init__.py
@@ -28,22 +28,3 @@ def rm_r(path):
         os.unlink(path)
     else:
         shutil.rmtree(path)
-
-def Sifre(S):
-    i = 0
-    j = 0
-    while True:
-        i = (i + 1) % 256
-        j = (j + S[i]) % 256
-        S[i], S[j] = S[j], S[i]
-        yield S[(S[i] + S[j]) % 256]
-
-def Sifrele(yazi, key, hexformat=False):
-    key, yazi = bytearray(key), bytearray(yazi)
-    S = list(range(256))
-    j = 0
-    for i in range(256):
-        j = (j + S[i] + key[i % len(key)]) % 256
-        S[i], S[j] = S[j], S[i]
-    keystream = Sifre(S)
-    return b''.join(b"%02X" % (c ^ next(keystream)) for c in yazi) if hexformat else bytearray(c ^ next(keystream) for c in yazi)
