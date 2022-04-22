9b56d02ee8d4cee0b486e24882f9bf3e06c49517
Nusret <89344065+nusrte@users.noreply.github.com>
2022-04-22 20:33:42 +0400

    Update language.py

--- merlin_installer/language.py
+++ merlin_installer/language.py
@@ -5,17 +5,17 @@ from . import logo, console, bilgi
 def importlang ():
     console.clear()
     logo()
-    bilgi("\n\[1] Türkçe\n\[2] Azərbaycanca\n\[3] English\n\[4] O'zbek")
+    bilgi("\n\[1] Azərbaycanca\n\[2] Türkçe\n\[3] English\n\[4] O'zbek")
     Dil = Prompt.ask("[bold yellow]Xahiş bir dil seçin / Please select a language[/]", choices=["1", "2", "3", "4"], default="1")
 
     if Dil == "1":
-        COUNTRY = "Turkey"
-        LANGUAGE = "TR"
-        TZ = "Europe/Istanbul"
-    elif Dil == "2":
         COUNTRY = "Azerbaijan"
         LANGUAGE = "AZ"
         TZ = "Asia/Baku"
+    elif Dil == "2":
+        COUNTRY = "Turkey"
+        LANGUAGE = "TR"
+        TZ = "Europe/Istanbul"
     elif Dil == "3":
         COUNTRY = "United Kingdom"
         LANGUAGE = "EN"
@@ -28,4 +28,4 @@ def importlang ():
     return COUNTRY, LANGUAGE, TZ
 
 COUNTRY, LANGUAGE, TZ = importlang()
-LANG = loads(open(f"./asena_installer/language/{LANGUAGE}.asenajson", "r").read())["STRINGS"]
+LANG = loads(open(f"./merlin_installer/language/{LANGUAGE}.merlinjson", "r").read())["STRINGS"]
