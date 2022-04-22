a9600101849854f48565e0c531c80c16e7a0d7bb
Nusret <89344065+nusrte@users.noreply.github.com>
2022-04-22 20:58:41 +0400

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,26 @@
+MESAJ="MerlinUserBot Avto Deploy Quruluma Xoş Gəldiniz"
+MESAJ+="\nTelegram: @MerlinUserBot"
+pkg update -y
+clear
+echo -e $MESAJ
+echo "Python Yüklənir"
+pkg install python -y
+clear
+echo -e $MESAJ
+echo "Git Yüklənir"
+pkg install git -y
+clear
+echo -e $MESAJ
+echo "TeleThon Yüklənir"
+pip install telethon
+echo "Repo klonlanır..."
+git clone https://github.com/Merlinuserb/Merlininstaller
+clear
+echo -e $MESAJ
+cd Merlininstaller
+clear
+echo "Bəzi lazımlı dosyalar yüklənir"
+echo -e $MESAJ
+pip install wheel
+pip install -r requirements.txt
+python -m merlin_installer
